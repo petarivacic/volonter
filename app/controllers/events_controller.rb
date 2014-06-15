ea2e08bef200bf392b018ac6120ba3806b3fc05b
Petar Ivacic <petar.ivacic@gmail.com>
2014-06-15 15:05:09 +0200

    sort

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.sort(created_at: -1)
   end
 
   # GET /events/1
